1e5dbfb3ab51f37e404a3b5bb1ae8ced5b826440
Labib Alam <labibalam1100@gmail.com>
2024-03-18 13:42:15 -0500

    Initial Commit

---
+++ Rock Paper Scissors/Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Mar 18, 2024
+ *      Author: labib
+ */
+
+#include "Main.h"
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Rock Paper Scissors/Main.h
@@ -0,0 +1,17 @@
+/*
+ * Main.h
+ *
+ *  Created on: Mar 18, 2024
+ *      Author: labib
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+class Main {
+public:
+	Main();
+	virtual ~Main();
+};
+
+#endif /* MAIN_H_ */
